7e1e9367841f33d663493fb9a884eac6de033230
michael tamsil <michaeltamsil@gmail.com>
2018-04-30 10:26:39 +0700

    update mata pelajaran

---
+++ front end/dev/app/mata_pelajaran/model.js
@@ -0,0 +1,15 @@
+define((require, exports, module) => {
+    'use strict'
+
+    const Backbone = require('backbone'),
+        config = require('config')
+
+    module.exports = Backbone.Model.extend({
+        urlRoot: `${config.urlAPI}mata_pelajaran`,
+        initialize(){
+            this.on('error', (model, response) => {
+                alert(response.statusText)
+            })
+        }
+    })
+})
---
+++ front end/dev/app/mata_pelajaran/update/view.js
@@ -0,0 +1,35 @@
+define((require, exports, module) => {
+    'use strict'
+
+    const LayoutManager = require('layoutmanager'),
+        template = require('text!./template.html'),
+        Model = require('./../model'),
+        Syphon = require('syphon')
+
+    module.exports = LayoutManager.extend({
+        className: 'row',
+        template: _.template(template),
+        initialize(){
+            this.model = new Model()
+            this.model.set('id', window.location.hash.split('/').pop())
+        },
+        events:{
+            'submit form': 'submitForm'
+        },
+        afterRender() {
+            this.model.once('sync', (model, data, response) => {
+                Syphon.deserialize(this, data)
+                this.model.once('sync', () => {
+                    let hashSplit = window.location.hash.split('/')
+                    hashSplit.pop()
+                    window.location.hash = `${hashSplit.join('/')}`
+                })
+            })
+            this.model.fetch()
+        },
+        submitForm(e){
+            e.preventDefault()
+            this.model.save(Syphon.serialize(this))
+        }
+    })
+})
